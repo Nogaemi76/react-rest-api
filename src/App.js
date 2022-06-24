1db0668759dcde5c760f4ef65180e9b07458c4f5
Emilie <milogare@yahoo.fr>
2022-06-24 18:03:04 +0200

    Added get request with fetch in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch(error => console.log(error.message));
+  }, []);
+
   return (
     <div className="App">
+      {posts.map(post => <div key={post.id}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </div>)}
     </div>
   );
 }
